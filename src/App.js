bacfa07fa58eda8fd7c6d08cfc3c8b26ecddcbcd
Julia  Martins  Maschion <julia.maschion@ifood.com.br>
2020-02-12 22:27:01 -0300

    modal in react

--- src/App.js
+++ src/App.js
@@ -1,26 +1,38 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './index.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Modal from "./Components/Modal";
+import React from "react";
+
+class App extends React.Component {
+
+  state = {
+    show: false,
+    hideButtonAndText: false
+  };
+  showModal = e => {
+    this.setState({
+      show: !this.state.show
+    });
+  };
+
+  handleChange = e => {
+    this.setState({
+    hideButtonAndText: !this.state.hideButtonAndText
+    });
+  };
+  
+    
+  render() {
+
+    const style = this.state.hideButtonAndText ? { display: "none"} : {}
+    return (
+      <div className="App">
+        <h1  handleChange={this.state.hideButtonAndText} style={style}>Modal</h1>
+        <button  handleChange={this.state.hideButtonAndText} style={style} class="toggle-button" id="centered-toggle-button" onClick={e => {this.showModal(e);}}>show Modal</button>
+        <Modal onClose={this.showModal} show={this.state.show}>
+          Message in Modal </Modal>
+      </div>
+    );
+  }
 }
-
 export default App;
---
+++ src/Components/Modal/index.js
@@ -0,0 +1,35 @@
+import "./modal.css";
+
+import PropTypes from "prop-types";
+import React from 'react';
+
+export default class Modal extends React.Component {
+
+  onClose = e => {
+    this.props.onClose && this.props.onClose(e);
+  };
+
+  render() {
+      if(!this.props.show){
+          return null;
+      }
+
+    return (
+      <div class="modal" id="modal">
+        <h2>Modal Window</h2>
+        <div class="content">{this.props.children}</div>
+        <div class="actions">
+          <button class="toggle-button" onClick={this.onClose}>
+            close
+          </button>
+        </div>
+      </div>
+  );
+    
+  }
+}
+
+Modal.propTypes = {
+  onClose: PropTypes.func.isRequired,
+  show: PropTypes.bool.isRequired
+};
